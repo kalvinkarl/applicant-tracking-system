c9e4827860aef0a648d720ddd3b7ecea69134458
Kalvin Karl C. Nonato <kalvinkarl28@outlook.com>
2022-07-16 05:22:31 +0800

    Prepare temporary host

--- src/app/components/applicant/applicant.component.ts
+++ src/app/components/applicant/applicant.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-applicant',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApplicantComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+	logout(): void {
+		this.authService.signOut();
+		this.router.navigate([""]);
+	}
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true
 };
+export const apiUrl = "https://vcylcnlvog.execute-api.ca-central-1.amazonaws.com/applicant-tracking-api";
